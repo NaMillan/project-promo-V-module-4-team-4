d7dc4f9a367605a0d87d9e72aab8de8de0c537f4
Teresa Toledo <laratoledoteresa@hotmail.com>
2024-02-19 12:32:27 +0100

    Add back

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+
+const server = express();
+
+server.use(cors());
+
+const port = 5001;
+
+server.listen(port, () => {
+	console.log(`El servidor se está ejecutando en el puerto ${port}`);
+});
+const staticServer = './web/dist';
+server.use(express.static(staticServer));
